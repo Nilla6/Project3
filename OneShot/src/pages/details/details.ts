d57fbd4bb4543f9a7520090177f76a6c653beb14
Kasey <kapowers@go.olemiss.edu>
2018-11-26 14:33:46 -0600

    working on rating--not working right

--- OneShot/src/pages/details/details.ts
+++ OneShot/src/pages/details/details.ts
@@ -1,16 +1,13 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AngularFireDatabase, AngularFireList} from "@angular/fire/database";
-import { NgModule } from '@angular/core';
-import { IonicPageModule } from 'ionic-angular';
-import { ParentPage } from './parent';
-import { StarRatingModule } from 'ionic3-star-rating';
 
 @IonicPage()
 @Component({
   selector: 'page-details',
   templateUrl: 'details.html',
 })
+
 export class DetailsPage {
 bars;
 
